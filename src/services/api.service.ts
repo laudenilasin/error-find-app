2e89220f92d349a23514b1dcc23e1c04bd487bc3
Lauden <laudenilasin@gmail.com>
2023-06-20 19:07:03 +0800

    Updated services

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,18 +1,20 @@
 import axios from "axios";
 
 export const getData = async <T>(): Promise<T> => {
-  const url = `http://localhost:4000/s3-data`;
+  const url = 'https://3ol4kpau8g.execute-api.ap-southeast-1.amazonaws.com/prod/proxy-server';
+  // const url = `http://localhost:4000/s3-data`;
 
   const result = await axios
     .get(url)
     .then((response) => {
+      console.log(response);
       return response.data.data;
     })
     .catch((error) => {
       console.error(error);
     });
 
-  console.log(result, "result!!!!!!!");
-
+    console.log(result);
   return result;
 };
+ 
